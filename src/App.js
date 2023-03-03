9025f20b6ed584d5bad02f2a163f9704b1877828
AJEET SINGH <ajeet.singh.cs23@iilmcet.ac.in>
2023-03-03 13:42:57 +0530

    Project is almost completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Linklist from "./Linklist";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>This is Header</header>
+      <div className="content">
+      <div className="aside">Manu Buttons</div>
+      <div><Linklist /></div>
+      </div>
+      
+      
     </div>
   );
 }
---
+++ src/Linklist.js
@@ -0,0 +1,73 @@
+function Linklist() {
+    return (
+        
+      <div className="Linklist">
+        <h1>Issues</h1>
+        <div className="description">Use the REST API to view and manage issues, including issue assignees, comments, labels, and milestones.</div>
+        <hr/>
+        <div>
+            <ul><a href="https://docs.github.com/en/rest/issues/issues">Issues</a>
+                
+                <li><a href="https://docs.github.com/en/rest/issues/issues#list-issues-assigned-to-the-authenticated-user">List Issues assigned to the authenticated user</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#list-organization-issues-assigned-to-the-authenticated-user">List organization issues assigned to the authenticated user</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#list-repository-issues">List repository issues</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#create-an-issue">Create an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#get-an-issue">Get an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#update-an-issue">Update an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#lock-an-issue">Lock an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#unlock-an-issue">Unlock an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/issues#list-user-account-issues-assigned-to-the-authenticated-user">List user account issues assigned to the authenticated user</a></li>
+            
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/assignees">Issue assignees</a>
+                <li><a href="https://docs.github.com/en/rest/issues/assignees#list-assignees">List assignees</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/assignees#check-if-a-user-can-be-assigned">Check if a user can be assigned</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/assignees#add-assignees-to-an-issue">Add assignees to an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/assignees#remove-assignees-from-an-issue">Remove assignees from an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/assignees#check-if-a-user-can-be-assigned-to-a-issue">Check if a user can be assigned to a issue</a></li>
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/comments">Issue comments</a>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#list-issue-comments-for-a-repository">List issue comments for a repository</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#get-an-issue-comment">Get an issue comment</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#update-an-issue-comment">Update an issue comment</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#delete-an-issue-comment">Delete an issue comment</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#list-issue-comments">List issue comment</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/comments#create-an-issue-comment">Create an issue comment</a></li>
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/events">Issue events</a>
+                <li><a href="https://docs.github.com/en/rest/issues/events#list-issue-events-for-a-repository">List issue events for a repository</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/events#get-an-issue-event">Get an issue event</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/events#list-issue-events">List issue events</a></li>
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/labels">Labels</a>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#list-labels-for-an-issue">List labels for an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#add-labels-to-an-issue">Add labels for an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#set-labels-for-an-issue">Set labels for an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#remove-all-labels-from-an-issue">Remove all labels from an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#remove-a-label-from-an-issue">Remove a label from an issue</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#list-labels-for-a-repository">List labels for a repository</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#create-a-label">Create a label</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#get-a-label">Get a label</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#update-a-label">Update a label</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#delete-a-label">Delete a label</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/labels#list-labels-for-issues-in-a-milestone">List labels for issues in a milestone</a></li>
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/milestones">Milestones</a>
+                <li><a href="https://docs.github.com/en/rest/issues/milestones#list-milestones">List milestones</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/milestones#create-a-milestone">Create a milestone</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/milestones#get-a-milestone">Get a milestone</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/milestones#update-a-milestone">Update a milestone</a></li>
+                <li><a href="https://docs.github.com/en/rest/issues/milestones#delete-a-milestone">Delete a milestone</a></li>
+            </ul>
+            <ul><a href="https://docs.github.com/en/rest/issues/timeline">Timeline events</a>
+                <li><a href="https://docs.github.com/en/rest/issues/timeline#list-timeline-events-for-an-issue">List Timeline events for an issue</a></li>
+            </ul>
+        </div>
+       
+            
+        
+      </div>
+    );
+  }
+  
+  export default Linklist;
